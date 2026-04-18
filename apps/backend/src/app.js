5cb9225df98948f43a8dde47b8001ab5dfb6ff77
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2026-04-19 03:02:59 +0600

    cors updated

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite dev server access 
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
