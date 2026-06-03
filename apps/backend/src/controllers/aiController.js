f2d531d0c180feee3acaea24b2825c15c947ec9f
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2026-06-03 20:30:44 +0600

    added redis cache system for AI chatbot, so that frequent questions are immediatly responded from cache without going to the LLM

--- apps/backend/src/controllers/aiController.js
+++ apps/backend/src/controllers/aiController.js
@@ -1,10 +1,28 @@
 const { Pool } = require('pg');
 const { generateEmbedding, getGroqChatResponse } = require('../services/aiService');
+const { createClient } = require('redis');
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL?.includes('neon.tech') || process.env.NODE_ENV === 'production'
   ? {rejectUnauthorized: false} : false
  });
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6329',
+  socket: {
+    // This tells node-redis to accept the encrypted TLS connection stream safely
+    tls: true,
+    rejectUnauthorized: false // Helps avoid handshake failures across distributed Render networks
+  }
+});
+
+redisClient.on('error', err => console.error('Redis Client Error', err));
+
+(async () => {
+  await redisClient.connect();
+  console.log('Connected to Secure Upstash Redis Cache successfully!');
+})();
+
 
 const handleChat = async (req,res)=>{
   const {message} = req.body;
@@ -17,7 +35,18 @@ const handleChat = async (req,res)=>{
       error: "Message too long. Please keep your question under 500 characters." 
     });
   }
+  console.log("message length is OK")
+  const cacheKey = `chat:${message.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~?]/g, "").trim()}`;
   try{
+    const cachedResponse = await redisClient.get(cacheKey);
+    if (cachedResponse) {
+      console.log(`[Redis Cache Hit] Serving data instantly`);
+      // Redis only stores strings, so we must parse it back into a JSON object
+      return res.json(JSON.parse(cachedResponse));
+    }
+    console.log("redis cache miss!")
+
+    console.log(`[Redis Cache Miss] Proceeding to Neon DB and Groq API`);
     const queryVector = await generateEmbedding(message);
     //make changes  :old
     // const dbResult = await pool.query(
@@ -42,10 +71,17 @@ const handleChat = async (req,res)=>{
       `Landmark: ${r.landmark_name} is served by Official Station: ${r.official_station_name}. Info: ${r.description}`
     ).join("\n");
     const reply = await getGroqChatResponse(message,context);
-    res.json({
-      reply,
-      context_used: dbResult.rows   //showing result 
+    //may delete
+    const finalResponse = { reply, context_used: dbResult.rows };
+    //add to redis
+    await redisClient.set(cacheKey, JSON.stringify(finalResponse), {
+      EX: 3600 
     });
+    // res.json({
+    //   reply,
+    //   context_used: dbResult.rows   //showing result 
+    // });
+    res.json(finalResponse);
   }catch(err){
     console.error("CHAT ERROR: ",err.message);
     res.status(500).json({error: "AI not working somehow, Try again"})
